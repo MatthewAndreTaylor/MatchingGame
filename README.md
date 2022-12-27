09fc25190a053ebc833a01dc6a88aafb9938f47f
MatthewAndreTaylor <matthew.andre.taylor@gmail.com>
2022-12-26 20:11:13 -0500

    turn based input

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Matching Cards app
+# Matching Cards Game
 
-Run the `npm install` command and `expo start` to start the app
+🐢 Run the `npm install` command and `expo start` to start the app.
 
-Setup a new firebase project and real-time database and import the security rules
+🚀 Setup a new firebase project and real-time database and import the security rules from `rules.txt`.
+
+Match card tiles with the same icon to cmplete the board.
 
--- components/PlayingCard.js
+++ components/PlayingCard.js
@@ -19,20 +19,35 @@ const map = {
   t: "free-breakfast",
 };
 
+let players = {};
+const allPlayersRef = ref(db, `players`);
+onValue(allPlayersRef, (snapshot) => {
+  players = snapshot.val() || {};
+  // Update turn when a new player joins (new)
+  update(gameRef, { turnId: global.playerId });
+});
+
 const PlayingCard = ({ index, cardIcon, cardColour }) => {
   const anim = useRef(new Animated.ValueXY());
 
+  const [turn] = useObjectVal(child(gameRef, "turnId"));
   const [counter] = useObjectVal(child(gameRef, "counter"));
 
   // Process a move
   const move = useCallback(() => {
+    // You cannot not move if it is not your turn
+    if (turn != global.playerId){
+      Animated.loop(shake, { iterations: 1 }).start();
+      return;
+    }
+
     if (counter === 0) {
       update(gameRef, { card1: index, counter: 1 });
     } else {
       update(gameRef, { card2: index, counter: 0 });
-      update(gameRef, { card1: {}, card2: {} });
+      update(gameRef, { card1: {}, card2: {}, turnId: nextPlayer(turn) });
     }
-  }, [counter]);
+  }, [counter, turn]);
 
   useEffect(() => {
     // Shake if the card is red
@@ -114,6 +129,15 @@ const PlayingCard = ({ index, cardIcon, cardColour }) => {
     </View>
   );
 };
+
+// Return the next playerId
+function nextPlayer(curr) {
+  let keys = Object.keys(players);
+  let nextIndex = keys.indexOf(curr) + 1;
+  let nextItem = keys[nextIndex];
+  if (nextItem != null) return nextItem;
+  return keys[0];
+}
 
 const Card = styled.Pressable`
     margin: 5px;
